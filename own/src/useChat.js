052d9c77841358afacf37cad212673db38d159da
Ric Huang <cyhuang@ntu.edu.tw>
2020-11-25 23:03:48 +0800

    Initial commit

---
+++ own/src/useChat.js
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+// import { w3cwebsocket as W3CWebSocket } from 'websocket'
+
+// const client = new W3CWebSocket('ws://localhost:4000')
+const client = new WebSocket('ws://localhost:4000')
+
+const useChat = () => {
+  const [messages, setMessages] = useState([])
+  const [status, setStatus] = useState({})
+  const [opened, setOpened] = useState(false)
+
+  client.onmessage = (message) => {
+    const { data } = message
+    const [task, payload] = JSON.parse(data)
+
+    switch (task) {
+      case 'init': {
+        setMessages(() => payload)
+        break
+      }
+      case 'output': {
+        setMessages(() => [...messages, ...payload])
+        break
+      }
+      case 'status': {
+        setStatus(payload)
+        break
+      }
+      case 'cleared': {
+        setMessages([])
+        break
+      }
+      default:
+        break
+    }
+  }
+
+  client.onopen = () => {
+    setOpened(true)
+  }
+
+  const sendData = (data) => {
+    // TODO
+  }
+
+  const sendMessage = (msg) => {
+    // TODO
+  }
+
+  const clearMessages = () => {
+    // TODO
+  }
+
+  return {
+    status,
+    opened,
+    messages,
+    sendMessage,
+    clearMessages
+  }
+}
+
+export default useChat
+
